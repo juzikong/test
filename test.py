66a9ec74bbd4e803a082df47c9fe18be90404d10
juzikong <beijingjuzikong@gmail.com>
2020-03-10 16:22:53 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 import math
+import gc
+import sys
+import warnings
+import os
+import errno
+import numpy as np
+import time
+from scipy.integrate import quad
+import importlib
 
 a = math.log(100, 10)
 
@@ -7,5 +16,5 @@ print(a)
 print("...text output OK...")
 
 file = open('testoutputfile.txt', 'w')
-file.write("test\n")
+file.write("out put file generated. \n\n===\n\n")
 file.close()
